7db8ecf39fa45881678e51a04949e9d23e2b9d51
Jeff Zhou <jlz45@cornell.edu>
2023-04-10 17:56:55 -0400

    initial tilesets (warning: ugly)

---
+++ objects.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="objects" tilewidth="40" tileheight="64" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="objects/button.png"/>
+ </tile>
+ <tile id="3">
+  <image width="40" height="34" source="objects/cat.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="64" source="objects/checkpoint.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="objects/spikes.png"/>
+ </tile>
+</tileset>
