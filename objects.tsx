55b6748e84c01f5927fdf42f26e4704bed54015b
Jeff Zhou <jlz45@cornell.edu>
2023-04-10 18:26:45 -0400

    test level with polygon walls

--- objects.tsx
+++ objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="objects" tilewidth="40" tileheight="64" tilecount="4" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="objects" tilewidth="40" tileheight="64" tilecount="4" columns="0" objectalignment="topleft">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="objects/button.png"/>
